adcea3a4a825f0335b72a439e7094d0e08caa018
SanBingYouYong <rayzhang707@gmail.com>
2023-11-19 21:47:21 +0000

    updated ui and added analysis and date/file specification of data sources; working!!!

--- app/src/main/java/com/specknet/pdiotapp/ReadCsvActivity.kt
+++ app/src/main/java/com/specknet/pdiotapp/ReadCsvActivity.kt
@@ -66,6 +66,10 @@ class ReadCsvActivity : AppCompatActivity() {
             if (dataList.isEmpty()) {
                 showNoDataAlert(this)
             }
+            val updatedSensorData = updateCsvData(dataList)
+            Log.d(TAG, "updatedSensorData = ${updatedSensorData.size}")
+            val analysis = produceAnalysis(updatedSensorData)
+            updateAnalysis(analysis)
 
             Log.d(TAG, "dataList = $dataList")
             fileAdapter = FileAdapter(dataList) { file ->
@@ -73,6 +77,8 @@ class ReadCsvActivity : AppCompatActivity() {
                 val sub_csv = Csv[0]
                 Log.d(TAG, "Csv = $sub_csv")
                 val sensorDataList = Csv
+                val single_analysis = produceAnalysis(sensorDataList)
+                updateAnalysis(single_analysis)
                 sensorDataAdapter = SensorDataAdapter(sensorDataList)
                 recyclerView.adapter = sensorDataAdapter
                 // 处理文件点击事件，例如打开文件
@@ -94,6 +100,10 @@ class ReadCsvActivity : AppCompatActivity() {
         if (dataList.isEmpty()) {
             showNoDataAlert(this)
         }
+        val updatedSensorData = updateCsvData(dataList)
+        Log.d(TAG, "updatedSensorData = ${updatedSensorData.size}")
+        val analysis = produceAnalysis(updatedSensorData)
+        updateAnalysis(analysis)
 
         Log.d(TAG, "dataList = $dataList")
         fileAdapter = FileAdapter(dataList) { file ->
@@ -101,6 +111,8 @@ class ReadCsvActivity : AppCompatActivity() {
             val sub_csv = Csv[0]
             Log.d(TAG, "Csv = $sub_csv")
             val sensorDataList = Csv
+            val single_analysis = produceAnalysis(sensorDataList)
+            updateAnalysis(single_analysis)
             sensorDataAdapter = SensorDataAdapter(sensorDataList)
             recyclerView.adapter = sensorDataAdapter
             // 处理文件点击事件，例如打开文件
@@ -109,11 +121,110 @@ class ReadCsvActivity : AppCompatActivity() {
         recyclerView.adapter = fileAdapter
     }
 
-//    private fun displayDataForDate(date: String) {
-//        val dataList = readCsvData(date)
-//        // Update your ListView or RecyclerView here with dataList
-//    }
-//
+    private fun updateAnalysis(analysis: Map<String, Float>) {
+        val mainActTypeTextView = findViewById<TextView>(R.id.mainTextView)
+        val mainTypeStr = analysis.keys.toList()[0] + ": " + "%.1f%%".format(analysis.values.toList()[0]*100)
+        val mainActSubtypeTextView = findViewById<TextView>(R.id.subTextView)
+        val mainSubtypeStr = analysis.keys.toList()[1] + ": " + "%.1f%%".format(analysis.values.toList()[1]*100)
+        val mainActTypeSubtypeTextView = findViewById<TextView>(R.id.totalTextView)
+        val mainTypeSubtypeStr = analysis.keys.toList()[2] + ": " + "%.1f%%".format(analysis.values.toList()[2]*100)
+        Log.d(TAG, "mainTypeStr = $mainTypeStr")
+        Log.d(TAG, "mainSubtypeStr = $mainSubtypeStr")
+        Log.d(TAG, "mainTypeSubtypeStr = $mainTypeSubtypeStr")
+        runOnUiThread {
+            mainActTypeTextView.text = mainTypeStr
+            mainActSubtypeTextView.text = mainSubtypeStr
+            mainActTypeSubtypeTextView.text = mainTypeSubtypeStr
+        }
+    }
+
+
+    /*
+        Get largest portions of activity type, subtypes, and both
+     */
+    private fun produceAnalysis(sensorDatas: MutableList<SensorData>): Map<String, Float> {
+        if (sensorDatas.isEmpty()) {
+            val nullMap = mutableMapOf<String, Float>()
+            nullMap["No Activity"] = 0.0f
+            nullMap["No Subtype"] = 0.0f
+            nullMap["No Data"] = 0.0f
+            return nullMap
+        }
+        val activityTypeMap = mutableMapOf<String, Int>()
+        val activitySubtypeMap = mutableMapOf<String, Int>()
+        val activityTypeSubtypeMap = mutableMapOf<String, Int>()
+
+        for (sensorData in sensorDatas) {
+            val activityType = sensorData.activityType
+            val activitySubtype = sensorData.activitySubtype
+            val activityTypeSubtype = "$activityType-$activitySubtype"
+
+            // Update activityTypeMap
+            if (activityTypeMap.containsKey(activityType)) {
+                activityTypeMap[activityType] = activityTypeMap[activityType]!! + 1
+            } else {
+                activityTypeMap[activityType] = 1
+            }
+
+            // Update activitySubtypeMap
+            if (activitySubtypeMap.containsKey(activitySubtype)) {
+                activitySubtypeMap[activitySubtype] = activitySubtypeMap[activitySubtype]!! + 1
+            } else {
+                activitySubtypeMap[activitySubtype] = 1
+            }
+
+            // Update activityTypeSubtypeMap
+            if (activityTypeSubtypeMap.containsKey(activityTypeSubtype)) {
+                activityTypeSubtypeMap[activityTypeSubtype] = activityTypeSubtypeMap[activityTypeSubtype]!! + 1
+            } else {
+                activityTypeSubtypeMap[activityTypeSubtype] = 1
+            }
+        }
+
+        // Sort the maps by value
+        val sortedActivityTypeMap = activityTypeMap.toList().sortedByDescending { (_, value) -> value }.toMap()
+        val sortedActivitySubtypeMap = activitySubtypeMap.toList().sortedByDescending { (_, value) -> value }.toMap()
+        val sortedActivityTypeSubtypeMap = activityTypeSubtypeMap.toList().sortedByDescending { (_, value) -> value }.toMap()
+
+        // Get the top activity types
+        val topActivityType = sortedActivityTypeMap.keys.first()
+        val topActivitySubtype = sortedActivitySubtypeMap.keys.first()
+        val topActivityTypeSubtype = sortedActivityTypeSubtypeMap.keys.first()
+
+        // Get the top activity types and their counts
+        val topActivityTypeCount = sortedActivityTypeMap[topActivityType]
+        val topActivitySubtypeCount = sortedActivitySubtypeMap[topActivitySubtype]
+        val topActivityTypeSubtypeCount = sortedActivityTypeSubtypeMap[topActivityTypeSubtype]
+
+        // Calculate the portions
+        val topActivityTypePortion = topActivityTypeCount!! / sensorDatas.size.toFloat()
+        val topActivitySubtypePortion = topActivitySubtypeCount!! / sensorDatas.size.toFloat()
+        val topActivityTypeSubtypePortion = topActivityTypeSubtypeCount!! / sensorDatas.size.toFloat()
+
+        // return these value pairs
+        val results = mutableMapOf<String, Float>()
+        results[topActivityType] = topActivityTypePortion
+        results[topActivitySubtype] = topActivitySubtypePortion
+        results[topActivityTypeSubtype] = topActivityTypeSubtypePortion
+        Log.d(TAG, "results = $results")
+        return results
+    }
+
+    // Date: [xx.csv, xxx, xxx]
+    // xx.csv: [xx.csv]
+    /*
+        read all included csv files and return list of data
+     */
+    private fun updateCsvData(datafiles: List<File>): MutableList<SensorData> {
+        val dataList = mutableListOf<SensorData>()
+        for (datafile in datafiles) {
+            val cur_datalist = readCsvFile(datafile.absolutePath)
+            // add elements to dataList
+            dataList.addAll(cur_datalist)
+        }
+        return dataList
+    }
+
     private fun readCsvData(date: String): List<File> {
         val dataList = mutableListOf<File>()
 
@@ -139,36 +250,10 @@ class ReadCsvActivity : AppCompatActivity() {
                 }
             }
         }
-
-//        val file = File(getExternalFilesDir(null), filename)
-//        val date = filename.split("_")[2].split("-")
-//        val day = date[0]
-//        val month = date[1]
-//        val year = date[2]
-//        val date = "$day/$month/$year"
-
-//        val bufferedReader = BufferedReader(FileReader(file))
-//        bufferedReader.useLines { lines ->
-//            lines.forEach { line ->
-//                if (line.contains(date)) {
-//                    // Parse the line into YourDataModel and add to dataList
-//                }
-//            }
-//        }
         return dataList
 }
 
     fun showNoDataAlert(context: Context) {
-//        val alertDialog = AlertDialog.Builder(context)
-//            .setTitle("Warning")
-//            .setMessage("No data for this date")
-//            .setPositiveButton("accept") { dialog, which ->
-//                // 这里可以处理确定按钮的点击事件
-//            }
-//            .setNegativeButton("cancel") { dialog, which ->
-//                // 这里可以处理取消按钮的点击事件
-//            }
-//            .create()
         Toast.makeText(this, "No Data for this date", Toast.LENGTH_SHORT).show()
 //        alertDialog.show()
     }
